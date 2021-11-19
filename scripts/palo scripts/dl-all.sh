bb3edc0406b4db5823f771c992ec5201ccbb6231
jfisher009 <60157369+jfisher009@users.noreply.github.com>
2021-11-19 12:06:12 -0600

    Update dl-all.sh

--- scripts/palo scripts/dl-all.sh
+++ scripts/palo scripts/dl-all.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+mkdir scripts
+cd scripts
 wget https://raw.githubusercontent.com/bryanlikescode/hotsauce/main/scripts/palo%20scripts/1-make-palo-addresses.txt
 wget https://raw.githubusercontent.com/bryanlikescode/hotsauce/main/scripts/palo%20scripts/2-make-palo-services.txt
 wget https://raw.githubusercontent.com/bryanlikescode/hotsauce/main/scripts/palo%20scripts/3-make-palo-service-groups.txt
+cd ..
+mkdir templates
+cd templates
+wget https://raw.githubusercontent.com/bryanlikescode/hotsauce/main/Write%20Up%20Templates/Blank%20Incident%20Response%20-%20Blank.docx
+wget https://raw.githubusercontent.com/bryanlikescode/hotsauce/main/Write%20Up%20Templates/Basic%20Memo%20-%20Blank.docx
+wget https://raw.githubusercontent.com/bryanlikescode/hotsauce/main/Write%20Up%20Templates/Password%20Policy%20Sample.docx
+wget https://raw.githubusercontent.com/bryanlikescode/hotsauce/main/Write%20Up%20Templates/Physical%20Security%20Memo%20.docx
+wget https://raw.githubusercontent.com/bryanlikescode/hotsauce/main/Write%20Up%20Templates/Logon%20Banner.txt
