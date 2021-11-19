bffd461ac4436d06f7f72f5a1b2fe8063fcaafec
jfisher009 <60157369+jfisher009@users.noreply.github.com>
2021-11-19 14:36:46 -0600

    Update dl-all.sh

--- scripts/palo scripts/dl-all.sh
+++ scripts/palo scripts/dl-all.sh
@@ -4,6 +4,7 @@ cd scripts
 wget https://raw.githubusercontent.com/bryanlikescode/hotsauce/main/scripts/palo%20scripts/1-make-palo-addresses.txt
 wget https://raw.githubusercontent.com/bryanlikescode/hotsauce/main/scripts/palo%20scripts/2-make-palo-services.txt
 wget https://raw.githubusercontent.com/bryanlikescode/hotsauce/main/scripts/palo%20scripts/3-make-palo-service-groups.txt
+wget https://raw.githubusercontent.com/bryanlikescode/hotsauce/main/scripts/palo%20scripts/4-make-simple-policies.txt
 cd ..
 mkdir templates
 cd templates
